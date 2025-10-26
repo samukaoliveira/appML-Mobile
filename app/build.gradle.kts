ef0ad09c144d279d584d08998d1f2f94fc56886e
Samuel <mucabatera@gmail.com>
2025-10-26 17:59:35 -0300

    Teste de atualização 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.appml"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.6.0"
+        versionCode = 6
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
